6b08952379129c38969fa26471bc5f4aecb1d66a
netBook <kav2019@mail.ru>
2023-08-31 10:02:51 +0300

    create progect

---
+++ Cloud file storage/src/test/java/ru/kovshov/cloud/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kovshov.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
