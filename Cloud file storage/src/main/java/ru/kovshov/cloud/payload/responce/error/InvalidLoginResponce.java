b4389e7f532c6968ee859b69d9ba76c3b36e5368
Alexandr <Alexandr>
2023-09-03 22:49:40 +0300

    add invalid login responce

---
+++ Cloud file storage/src/main/java/ru/kovshov/cloud/payload/responce/error/InvalidLoginResponce.java
@@ -0,0 +1,10 @@
+package ru.kovshov.cloud.payload.responce.error;
+
+public class InvalidLoginResponce {
+    private String name = "invalid name";
+    public String password = "invalid name";
+
+
+
+
+}
--- Cloud file storage/src/main/java/ru/kovshov/cloud/security/old/JWTAuthenticationEntryPoint.java
+++ Cloud file storage/src/main/java/ru/kovshov/cloud/security/old/JWTAuthenticationEntryPoint.java
@@ -1,6 +1,18 @@
 package ru.kovshov.cloud.security.old;
 
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import ru.kovshov.cloud.payload.responce.error.InvalidLoginResponce;
+import ru.kovshov.cloud.security.SecurityConstants;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+
+import javax.security.sasl.AuthenticationException;
+import java.io.IOException;
 
 @Component
 public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
