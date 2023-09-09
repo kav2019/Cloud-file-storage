c810b50765a947aa7b06819210cbfa6217aa0d7c
netBook <kav2019@mail.ru>
2023-09-09 22:42:36 +0300

    add notificationService angular

--- front/src/app/auth/register/register.component.ts
+++ front/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../service/auth.service";
+import {NotificationService} from "../../service/notification.service";
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  public registerForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private fb: FormBuilder
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.createRegisterForm();
+  }
+
+  createRegisterForm(): FormGroup {
+    return this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  submit(): void {
+    console.log(this.registerForm.value);
+
+    this.authService.register({
+      email: this.registerForm.value.email,
+      username: this.registerForm.value.username,
+      password: this.registerForm.value.password,
+    }).subscribe(data => {
+      console.log(data);
+      this.notificationService.showSnackBar('Successfully Registered!');
+    }, error => {
+      this.notificationService.showSnackBar('Something went wrong during registration');
+    });
+  }
 }
