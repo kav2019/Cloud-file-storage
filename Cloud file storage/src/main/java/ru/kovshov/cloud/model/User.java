07e2b8192e84f8b32f1cac865e27654d9223dd5d
kav2019 <kav2019@mail.ru>
2023-09-19 12:07:28 +0300

    debug user model

--- Cloud file storage/src/main/java/ru/kovshov/cloud/model/User.java
+++ Cloud file storage/src/main/java/ru/kovshov/cloud/model/User.java
@@ -37,6 +37,7 @@ public class User implements UserDetails {
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
+    @Column(name = "role")
     private Set<Role> userRole = new HashSet<>();
 
 //    @Override
